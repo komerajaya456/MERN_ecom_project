e90d26626256963299b3f6a8eefe27ae906d6f44
komerajaya456 <komerajaya7259@gmail.com>
2023-12-22 00:56:15 +0530

    added upper navbar to register page,&& router for local..3000/register using react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,15 @@
 import Register from "./components/register";
+import {BrowserRouter as Router,Routes,Route} from 'react-router-dom';
 
 function App() {
   return (
     <>
-      <Register />
+    <Router>
+      <Routes>
+      <Route path='/register' Component={Register}/>
+      </Routes>
+    </Router>
+
     </>
   );
 }
--- client/src/components/register.js
+++ client/src/components/register.js
@@ -3,48 +3,77 @@ import "../css/register.css";
 function Register() {
   return (
     <>
-      <div className="regform">
-        <form>
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container-fluid">
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon" />
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Register
+                </a>
+              </li>
+            </ul>
+            <form className="d-flex" role="search">
+              <a className="nav-link" href="#">
+                Logout
+              </a>
+            </form>
+          </div>
+        </div>
+      </nav>
+
+      <div className="regformdiv">
+        <form className="regformtag">
           <div className="mb-3">
-            <label  className="form-label">
-             Name
-            </label>
-            <input
-              type="text"
-              className="form-control"
-              id="exampleInputName"
-              
-            />
+            <label className="form-label"> Name</label>
+            <input type="text" className="form-control" id="exampleInputName" />
           </div>
 
           <div className="mb-3">
-            <label  className="form-label">
-              Gender
-            </label>
+            <label className="form-label"> Gender</label>
+            <select className="form-control">
+              <option>--select--</option>
+              <option>Female</option>
+              <option>Male</option>
+            </select>
+          </div>
+
+          <div className="mb-3">
+            <label className="form-label"> Email address</label>
             <input
               type="email"
               className="form-control"
               id="exampleInputEmail1"
-              
             />
           </div>
 
           <div className="mb-3">
-            <label  className="form-label">
-              Email address
-            </label>
+            <label className="form-label"> Password</label>
             <input
-              type="email"
+              type="password"
               className="form-control"
-              id="exampleInputEmail1"
-              
+              id="exampleInputPassword1"
             />
           </div>
 
           <div className="mb-3">
-            <label  className="form-label">
-              Password
-            </label>
+            <label className="form-label">Confirm Password</label>
             <input
               type="password"
               className="form-control"
